66fe2155ad8505943415313ee6ede7000a06cc3e
Bachar Daowd <bachar.daowd@gmail.com>
2022-06-29 22:51:21 +0200

    First Stp

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,32 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+const initialState = {
+  user: null,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: '',
+}
+
+//Register new Chefe 
+export const register = createAsyncThunk('auth/register', async(user, thunkAPI)=> {
+
+})
+
+//Login Chefe 
+export const login = createAsyncThunk('auth/login', async(user, thunkAPI)=> {
+
+})
+
+
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+
+  }
+})
+
+export default authSlice.reducer
